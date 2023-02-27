79fcd61b39fa271dbf1beca7f5677adab93734df
Jaikumar248 <jaikumaryh248@>
2023-02-27 21:08:33 +0530

    Formvalidations

---
+++ src/app/todoform/todoform.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { TodoService } from '../todo.service';
+
+@Component({
+  selector: 'app-todoform',
+  templateUrl: './todoform.component.html',
+  styleUrls: ['./todoform.component.css']
+})
+export class TodoformComponent {
+  todoInput:any = "";
+  constructor(private ts:TodoService){}
+  addTask(){
+    this.ts.addTodo(this.todoInput)
+    this.todoInput = "";
+
+  }
+  resetForm(){
+    this.todoInput=""
+  }
+}
